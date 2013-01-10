621f5c4472eb7efe41127306d8746dbf8a8e1626
Stuart Archibald <stuart@opengamma.com>
2013-01-10 12:13:07 +0000

    Start of some tests

---
+++ src/test/izymakecheck.h
@@ -0,0 +1,8 @@
+#ifndef IZYMAKECHECK_H
+#define IZYMAKECHECK_H
+#define _IOERROR 0x02
+#define _READERROR 0x03
+#define _MALLOCERROR 0x04
+#define _BADCOUNT 0x05
+#define _INCORRECTRESULT 0x06
+#endif
