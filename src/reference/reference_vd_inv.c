6bb49657a42822be253ce2630456d135466dab49
Stuart Archibald <stuart@opengamma.com>
2013-01-14 16:37:10 +0000

    e0 const forced

--- src/reference/reference_vd_inv.c
+++ src/reference/reference_vd_inv.c
@@ -4,5 +4,5 @@
 #include "izymathsmacros.h"
 void vd_reference_inv(double * arg0, const int * offsetarg0, double * out0, const int * offsetout0, const int * count)
 {
-        VFUNC(1/)
+        VFUNC(1.e0/)
 };
