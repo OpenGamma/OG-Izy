c58fa7c975642b33ee85fe1b5a5fdc3ea83b3588
Stuart Archibald <stuart@opengamma.com>
2013-01-29 14:02:55 +0000

    Renamed wrapper macros

--- src/reference/reference_vd_expm1.c
+++ src/reference/reference_vd_expm1.c
@@ -4,5 +4,5 @@
 #include "libizy/izymathsmacros.h"
 void vd_reference_expm1(const int * count, const double * arg0, const int * offsetarg0, double * out0, const int * offsetout0)
 {
-        VFUNC(expm1)
+        DVFUNC(expm1)
 };
--- src/reference/reference_vd_log1p.c
+++ src/reference/reference_vd_log1p.c
@@ -4,5 +4,5 @@
 #include "libizy/izymathsmacros.h"
 void vd_reference_log1p(const int * count, const double * arg0, const int * offsetarg0, double * out0, const int * offsetout0)
 {
-        VFUNC(log1p)
+        DVFUNC(log1p)
 };
