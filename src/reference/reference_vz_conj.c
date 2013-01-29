fb3a53a0442ed73ab9e31d9421f0f3cbd696d001
Stuart Archibald <stuart@opengamma.com>
2013-01-29 15:16:13 +0000

    reference impls of the complex functions in

---
+++ src/reference/reference_vz_conj.c
@@ -0,0 +1,8 @@
+//conj
+#include <math.h>
+#include "libizy/izyreference.h"
+#include "libizy/izymathsmacros.h"
+void vz_reference_conj(const int * count, const double complex * arg0, const int * offsetarg0, double complex * out0, const int * offsetout0)
+{
+        ZVFUNC(conj)
+};
---
+++ src/reference/reference_vz_pow.c
@@ -0,0 +1,8 @@
+//pow
+#include <math.h>
+#include "libizy/izyreference.h"
+#include "libizy/izymathsmacros.h"
+void vz_reference_pow(const int * count, const double complex * arg0, const int * offsetarg0, const double complex * arg1, const int * offsetarg1, double complex * out0, const int * offsetout0)
+{
+        ZVFUNC2ARG(cpow)
+};
