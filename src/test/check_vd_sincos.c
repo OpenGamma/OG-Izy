e762758a27aadb685c0ed2844ae931838c8d07c2
Stuart Archibald <stuart@opengamma.com>
2013-02-22 09:27:11 +0000

    Fixed unit tests so they #include generated data

--- src/test/check_vd_sincos.c
+++ src/test/check_vd_sincos.c
@@ -11,190 +11,100 @@
 
 
 int main()
-{       
-        const char * countfilename = "count_sincos.txt";
-        const char * inputfilename = "input_sincos.txt";        
-        const char * expectedfilename = "expected_sincos.txt";        
-        FILE * input = NULL;
-        int i, ret;
-               
-        input = fopen(countfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        int n_in = -1;
-        int n_expected = -1;        
-        ret=fscanf(input, "%d", &n_in);
-        if(!ret)
-        {
-                return _READERROR;
-        }
-        ret=fscanf(input, "%d", &n_expected);        
-        if(!ret)
-        {
-                return _READERROR;
-        }
-        fclose(input);
-        
-        if(n_in<1||n_expected<1)
-        {
-                return _BADCOUNT;
-        }
-        
-        /* Get input data */
-        input = NULL;
-        input = fopen(inputfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        double * in_data = NULL;
-        in_data = (double * ) malloc(n_in*sizeof(double));
-        if(!in_data)
-        {       
-                return _MALLOCERROR;
-        }
-        for(i=0; i<n_in;i++)
-        {
-                ret=fscanf(input,"%lf",&in_data[i]);
-                if(!ret)
-                {
-                        return _READERROR;
-                }
-        }
-        fclose(input);
-        
-        
-        /* Get results data */
-        input = NULL;
-        input = fopen(expectedfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        double * expected_data0 = NULL, * expected_data1 = NULL;
-        expected_data0 = (double * ) malloc((n_expected>>1)*sizeof(double));
-        expected_data1 = (double * ) malloc((n_expected>>1)*sizeof(double));        
-        if(!expected_data0)
-        {       
-                return _MALLOCERROR;
-        }
-        if(!expected_data1)
-        {       
-                return _MALLOCERROR;
-        }       
-        for(i=0; i<n_expected>>1;i++)
-        {
-                ret=fscanf(input,"%lf",&expected_data0[i]);
-                if(!ret)
-                {
-                        return _READERROR;
-                }
-                ret=fscanf(input,"%lf",&expected_data1[i]);                                
-                if(!ret)
-                {
-                        return _READERROR;
-                }                
-        }
-        fclose(input);
+{
+#include "vd_sincos_c.inc"
+  int i;
+  double * results_data0 = NULL, * results_data1 = NULL;
+  results_data0 = (double * ) malloc((n_expected)*sizeof(double));
+  if(!results_data0)
+    {
+      return _MALLOCERROR;
+    }
 
+  results_data1 = (double * ) malloc((n_expected)*sizeof(double));
+  if(!results_data1)
+    {
+      return _MALLOCERROR;
+    }
 
-        /* stage izy call */
-        double * results_data0 = NULL, * results_data1 = NULL;
-        results_data0 = (double * ) malloc((n_expected>>1)*sizeof(double));
-        if(!results_data0)
-        {       
-                return _MALLOCERROR;
-        }
+  const int offsetin0 = 0;
+  const int offsetout0 = 0;
+  const int offsetout1 = 0;
+  const int count = n_in;
 
-        results_data1 = (double * ) malloc((n_expected>>1)*sizeof(double));
-        if(!results_data1)
-        {       
-                return _MALLOCERROR;
-        }
-        
-        const int offsetin0 = 0;
-        const int offsetout0 = 0;
-        const int offsetout1 = 0;        
-        const int count = n_in;
-        
-        /* make izy call */
-        vd_sincos(&count,in_data,&offsetin0,results_data0,&offsetout0,results_data1,&offsetout1);
-        
-        /* check */
-        for(i=0; i<n_expected>>1;i++)
-        {
-                if(fabs(results_data0[i]-expected_data0[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }
-                if(fabs(results_data1[i]-expected_data1[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }                        
-        }
-        
-        /* /*stage offset call */
-        const int offsetin_used0 = (int)0.2*count;
-        const int offsetout_used0 = (int)0.4*count;
-        const int offsetout_used1 = (int)0.4*count;        
-        const int count_used = count - offsetout_used0;
-        memset(results_data0,0x0,count*sizeof(double));
-        memset(results_data1,0x0,count*sizeof(double));
-        
-        /* make izy call */       
-        vd_sincos(&count,in_data,&offsetin_used0,results_data0,&offsetout_used0,results_data1,&offsetout_used1);
+  /* make izy call */
+  vd_sincos(&count,in_data,&offsetin0,results_data0,&offsetout0,results_data1,&offsetout1);
 
-        /* check */
-        for(i=offsetout_used0; i < offsetout_used0+count_used;i++)
+  /* check */
+  for(i=0; i<n_expected; i++)
+    {
+      if(fabs(results_data0[i]-expected_data0[i])>=IZY_DBL_EPSILON)
         {
-                if(fabs(results_data0[i]-expected_data0[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }
-                if(fabs(results_data1[i]-expected_data1[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }                           
+          return _INCORRECTRESULT;
         }
+      if(fabs(results_data1[i]-expected_data1[i])>=IZY_DBL_EPSILON)
+        {
+          return _INCORRECTRESULT;
+        }
+    }
 
-        /* stage calls to saturation */
-        double IVAL =  0.e0;
-        double RVAL0 =  0.e0;        
-        double RVAL1 =  0.e0;
-        const int one = 1;
-               
-        /* test NaN arg0 */
-        IVAL = NAN;
-        vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
-        if(!isnan(RVAL0)||!isnan(RVAL1))
-        {
-                return _INCORRECTRESULT;
-        }
-        
-        /* test +INF */
-        IVAL = INFINITY;
-        vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
-        if(!isnan(RVAL0)||!isnan(RVAL1))
-        {
-               return _INCORRECTRESULT;
-        }
+  /* /*stage offset call */
+  const int offsetin_used0 = (int)0.2*count;
+  const int offsetout_used0 = (int)0.4*count;
+  const int offsetout_used1 = (int)0.4*count;
+  const int count_used = count - offsetout_used0;
+  memset(results_data0,0x0,count*sizeof(double));
+  memset(results_data1,0x0,count*sizeof(double));
 
-        /* test -INF */
-        IVAL = -INFINITY;        
-        vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
-        if(!isnan(RVAL0)||!isnan(RVAL1))
+  /* make izy call */
+  vd_sincos(&count,in_data,&offsetin_used0,results_data0,&offsetout_used0,results_data1,&offsetout_used1);
+
+  /* check */
+  for(i=offsetout_used0; i < offsetout_used0+count_used; i++)
+    {
+      if(fabs(results_data0[i]-expected_data0[i])>=IZY_DBL_EPSILON)
         {
-               return _INCORRECTRESULT;
+          return _INCORRECTRESULT;
         }
-        
-        free(in_data);
-        free(expected_data0);
-        free(expected_data1);                
-        free(results_data0);
-        free(results_data1);        
-        return 0;
+      if(fabs(results_data1[i]-expected_data1[i])>=IZY_DBL_EPSILON)
+        {
+          return _INCORRECTRESULT;
+        }
+    }
+
+  /* stage calls to saturation */
+  double IVAL =  0.e0;
+  double RVAL0 =  0.e0;
+  double RVAL1 =  0.e0;
+  const int one = 1;
+
+  /* test NaN arg0 */
+  IVAL = NAN;
+  vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
+  if(!isnan(RVAL0)||!isnan(RVAL1))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  /* test +INF */
+  IVAL = INFINITY;
+  vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
+  if(!isnan(RVAL0)||!isnan(RVAL1))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  /* test -INF */
+  IVAL = -INFINITY;
+  vd_sincos(&one,&IVAL,&offsetin0,&RVAL0,&offsetout0,&RVAL1,&offsetout1);
+  if(!isnan(RVAL0)||!isnan(RVAL1))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  free(results_data0);
+  free(results_data1);
+  return 0;
 }
 
 
--- src/test/check_vd_subx.c
+++ src/test/check_vd_subx.c
@@ -11,253 +11,162 @@
 
 
 int main()
-{       
-        const char * countfilename = "count_subx.txt";
-        const char * inputfilename = "input_subx.txt";        
-        const char * expectedfilename = "expected_subx.txt";        
-        FILE * input = NULL;
-        int i, ret;
-               
-        input = fopen(countfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        int n_in = -1;
-        int n_expected = -1;        
-        ret=fscanf(input, "%d", &n_in);
-        if(!ret)
-        {
-                return _READERROR;
-        }
-        ret=fscanf(input, "%d", &n_expected);        
-        if(!ret)
-        {
-                return _READERROR;
-        }
-        fclose(input);
-        
-        if(n_in<1||n_expected<1)
-        {
-                return _BADCOUNT;
-        }
-        
-        /* Get input data */
-        input = NULL;
-        input = fopen(inputfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        double * in_data0 = NULL, * in_data1 = NULL;
-        in_data0 = (double * ) malloc((n_in>>1)*sizeof(double));
-        in_data1 = (double * ) malloc(sizeof(double));        
-        if(!in_data0)
-        {       
-                return _MALLOCERROR;
-        }
-        if(!in_data1)
-        {       
-                return _MALLOCERROR;
-        }       
-        for(i=0; i<n_in>>1;i++)
-        {
-                ret=fscanf(input,"%lf",&in_data0[i]);
-                if(!ret)
-                {
-                        return _READERROR;
-                }
-                ret=fscanf(input,"%lf",in_data1);
-                if(!ret)
-                {
-                        return _READERROR;
-                }        
+{
+#include "vd_subx_c.inc"
+  int i;
+  double * results_data = NULL;
+  results_data = (double * ) malloc(n_expected*sizeof(double));
+  if(!results_data)
+    {
+      return _MALLOCERROR;
+    }
 
-                        
-        }
-        fclose(input);
-        
-        
-        /* Get results data */
-        input = NULL;
-        input = fopen(expectedfilename,"r");
-        if(!input)
-        {
-                return _IOERROR;
-        }
-        double * expected_data = NULL;
-        expected_data = (double * ) malloc(n_expected*sizeof(double));
-        if(!expected_data)
-        {       
-                return _MALLOCERROR;
-        }
-        for(i=0; i<n_expected;i++)
-        {
-                ret=fscanf(input,"%lf",&expected_data[i]);
-                if(!ret)
-                {
-                        return _READERROR;
-                }
-        }
-        
-        fclose(input);
+  const int offsetin0 = 0;
+  const int offsetin1 = 0;
+  const int offsetout0 = 0;
+  const int count = n_in;
+  memset(results_data,0x0,count*sizeof(double));
 
-        /* stage izy call */
-        double * results_data = NULL;
-        results_data = (double * ) malloc(n_expected*sizeof(double));
-        if(!results_data)
-        {       
-                return _MALLOCERROR;
-        }
-        
-        const int offsetin0 = 0;
-        const int offsetin1 = 0;        
-        const int offsetout0 = 0;
-        const int count = n_in>>1;
-        memset(results_data,0x0,count*sizeof(double));        
-        
-        /* make izy call */
-        vd_subx(&count,in_data0,&offsetin0,in_data1,&offsetin1,results_data,&offsetout0);
-        
-        /* check */
-        for(i=0; i<n_expected;i++)
-        {
-                if(fabs(results_data[i]-expected_data[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }
-                        
-        }
-        
-        /* stage offset call */
-        const int offsetin_used0 = (int)0.2*count;
-        const int offsetin_used1 = 0;
-        const int offsetout_used0 = (int)0.4*count;
-        const int count_used = count - offsetout_used0;
-        
-        /* make izy call */       
-        vd_subx(&count_used,in_data0,&offsetin_used0,in_data1,&offsetin_used1,results_data,&offsetout_used0);
+  /* make izy call */
+  vd_subx(&count,in_data0,&offsetin0,in_data1,&offsetin1,results_data,&offsetout0);
 
-        /* check */
-        for(i=offsetout_used0; i < offsetout_used0+count_used;i++)
+  /* check */
+  for(i=0; i<n_expected; i++)
+    {
+      if(fabs(results_data[i]-expected_data[i])>=IZY_DBL_EPSILON)
         {
-                if(fabs(results_data[i]-expected_data[i])>=IZY_DBL_EPSILON)
-                {
-                        return _INCORRECTRESULT;
-                }
-                        
+          return _INCORRECTRESULT;
         }
 
-          /* stage calls to saturation */
-        double IVAL0 =  0.e0;
-        double IVAL1 =  0.e0;        
-        double RVAL =  0.e0;
-        const int one = 1;
-               
-        /* test NaN arg0 */
-        IVAL0 = NAN;
-        IVAL1 =  0.e0;                
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
+    }
+
+  /* stage offset call */
+  const int offsetin_used0 = (int)0.2*count;
+  const int offsetin_used1 = 0;
+  const int offsetout_used0 = (int)0.4*count;
+  const int count_used = count - offsetout_used0;
+
+  /* make izy call */
+  vd_subx(&count_used,in_data0,&offsetin_used0,in_data1,&offsetin_used1,results_data,&offsetout_used0);
+
+  /* check */
+  for(i=offsetout_used0; i < offsetout_used0+count_used; i++)
+    {
+      if(fabs(results_data[i]-expected_data[i])>=IZY_DBL_EPSILON)
         {
-                return _INCORRECTRESULT;
-        }
-        
-        IVAL0 = NAN;
-        IVAL1 = INFINITY;         
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-                return _INCORRECTRESULT;
-        }
-       
-        IVAL0 = NAN;
-        IVAL1 = -INFINITY;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-                return _INCORRECTRESULT;
-        }
-        
-        IVAL0 = NAN;
-        IVAL1 = NAN;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-                return _INCORRECTRESULT;
-        }        
-              
-        /* test +INF */
-        IVAL0 = INFINITY;
-        IVAL1 =  0.e0;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!(isinf(RVAL)&&!signbit(RVAL)))
-        {
-               return _INCORRECTRESULT;
+          return _INCORRECTRESULT;
         }
 
-        IVAL0 = INFINITY;
-        IVAL1 = INFINITY;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-               return _INCORRECTRESULT;
-        }
-         
-        IVAL0 = INFINITY;
-        IVAL1 = -INFINITY;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!(isinf(RVAL)&&!signbit(RVAL)))
-        {
-               return _INCORRECTRESULT;
-        }        
-        
-        IVAL0 = INFINITY;
-        IVAL1 = NAN;        
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-               return _INCORRECTRESULT;
-        }               
-        
-        /* test -INF */
-        IVAL0 = -INFINITY;        
-        IVAL1 =  0.e0;
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!(isinf(RVAL)&&signbit(RVAL)))
-        {
-               return _INCORRECTRESULT;
-        }
-        
-        IVAL0 = -INFINITY;        
-        IVAL1 = INFINITY;
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!(isinf(RVAL)&&signbit(RVAL)))
-        {
-               return _INCORRECTRESULT;
-        }
-         
-        IVAL0 = -INFINITY;        
-        IVAL1 = -INFINITY;
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-               return _INCORRECTRESULT;
-        }        
-        
-        IVAL0 = -INFINITY;        
-        IVAL1 = NAN;
-        vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
-        if(!isnan(RVAL))
-        {
-               return _INCORRECTRESULT;
-        }                
-        
-        free(in_data0);
-        free(in_data1);        
-        free(expected_data);        
-        free(results_data);
-        return 0;
+    }
+
+  /* stage calls to saturation */
+  double IVAL0 =  0.e0;
+  double IVAL1 =  0.e0;
+  double RVAL =  0.e0;
+  const int one = 1;
+
+  /* test NaN arg0 */
+  IVAL0 = NAN;
+  IVAL1 =  0.e0;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = NAN;
+  IVAL1 = INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = NAN;
+  IVAL1 = -INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = NAN;
+  IVAL1 = NAN;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  /* test +INF */
+  IVAL0 = INFINITY;
+  IVAL1 =  0.e0;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!(isinf(RVAL)&&!signbit(RVAL)))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = INFINITY;
+  IVAL1 = INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = INFINITY;
+  IVAL1 = -INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!(isinf(RVAL)&&!signbit(RVAL)))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = INFINITY;
+  IVAL1 = NAN;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  /* test -INF */
+  IVAL0 = -INFINITY;
+  IVAL1 =  0.e0;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!(isinf(RVAL)&&signbit(RVAL)))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = -INFINITY;
+  IVAL1 = INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!(isinf(RVAL)&&signbit(RVAL)))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = -INFINITY;
+  IVAL1 = -INFINITY;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+  IVAL0 = -INFINITY;
+  IVAL1 = NAN;
+  vd_subx(&one,&IVAL0,&offsetin0,&IVAL1,&offsetin1,&RVAL,&offsetout0);
+  if(!isnan(RVAL))
+    {
+      return _INCORRECTRESULT;
+    }
+
+
+  free(results_data);
+  return 0;
 }
 
 
