35be1da44601424e7e92946409eb0e11d837ca28
Stuart Archibald <stuart@opengamma.com>
2013-03-14 16:20:25 +0000

    Complex vector trig tests

---
+++ src/test/check_vz_atanh.c
@@ -0,0 +1,52 @@
+#ifdef HAVE_CONFIG
+#include "libizy/config.h"
+#endif
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+#include "libizy/izy.h"
+#include "izymakecheck.h"
+
+// results are a bit fuzzy depending on implementation
+// TODO: this should be tightened in the future
+#ifdef IZY_MAX_ULPS
+#undef IZY_MAX_ULPS
+#define IZY_MAX_ULPS  100
+#endif
+
+int main()
+{
+#include "vz_atanh_c.inc"
+  int i;
+  complex double results_data[n_expected];
+
+  const int offsetin0 = 0;
+  const int offsetout0 = 0;
+  const int count = n_in;
+
+  /* make izy call */
+  vz_atanh(&count,in_data,&offsetin0,results_data,&offsetout0);
+
+  /* check */
+  for(i=0; i<n_expected; i++)
+    {
+      TEST_COMPLEX_DOUBLE_EQUALS_FULL(i, expected_data[i], results_data[i], IZY_MAX_ULPS, _STANDARD_LOOP_INCORRECT_RESULT)
+    }
+
+  /* stage offset call */
+  const int offsetin_used0 = (int)(0.2*count);
+  const int offsetout_used0 = (int)(0.4*count);;
+  const int count_used = count - offsetout_used0;
+  memset(results_data,0x0,count*sizeof(complex double));
+
+  /* make izy call */
+  vz_atanh(&count_used,in_data,&offsetin_used0,results_data,&offsetout_used0);
+
+  /* check */
+  for(i=0; i < count_used; i++)
+    {
+      TEST_COMPLEX_DOUBLE_EQUALS_FULL(i, expected_data[i+offsetin_used0], results_data[i+offsetout_used0], IZY_MAX_ULPS, _OFFSET_LOOP_INCORRECT_RESULT)
+    }
+  return 0;
+}
