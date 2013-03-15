f494ac9fdae04b81eff3da8ec9d6b92b6d41fbea
Stuart Archibald <stuart@opengamma.com>
2013-03-15 15:01:48 +0000

    Compatibility update. Scaled complex powx as it was ridiculous

--- src/test/gendata.m
+++ src/test/gendata.m
@@ -1,33 +1,33 @@
 function retstr = gendata(fnname, uname, lang, arg0, arg1)
   complexD=false;
-  if(any(iscomplex(arg0)))
+  if(any(~isreal(arg0)))
     complexD=true;
-    filetag="vz_";
+    filetag='vz_';
   else
-    filetag="vd_";
+    filetag='vd_';
   end
 
   if(~complexD)
-  disp(["REAL: Calling function: ",fnname," for IZY ",uname]);
+  disp(['REAL: Calling function: ',fnname,' for IZY ',uname]);
   else
-  disp(["CMPLX: Calling function: ",fnname," for IZY ",uname]);
+  disp(['CMPLX: Calling function: ',fnname,' for IZY ',uname]);
   end
 
   
 
   if(strcmp(lower(lang),'c'))
-    fname=[filetag,uname,"_c.inc"];
+    fname=[filetag,uname,'_c.inc'];
   elseif(strcmp(lower(lang),'java'))
-    fname=[filetag,uname,"_java.inc"];
+    fname=[filetag,uname,'_java.inc'];
   else
-    error("unknown language",lang);
+    error('unknown language',lang);
   end
 
-  retstr = ["@Test\n public void ",filetag,uname,"Test() {\n #include \"",fname,"\"\n"];
+  retstr = ['@Test\n public void ',filetag,uname,'Test() {\n #include "',fname,'"\n'];
 
   fp=fopen(fname,'w+');
   if(fp<0)
-    error("file open failed")
+    error('file open failed')
   end
 
   % functions starting x are of form const(OP)array so n_in depends on arg1
@@ -37,16 +37,18 @@ function retstr = gendata(fnname, uname, lang, arg0, arg1)
     fprintf(fp,'int n_in = %d;\n',numel(arg0));
   end
 
+  eval(['fnname_handle=@',fnname,';']);
+  
   if(nargin==4)
     writeData(fp,arg0,'in_data','',lang);
     out1=[];
     try
-            [out0,out1]=arrayfun(fnname,arg0);
+            [out0,out1]=arrayfun(fnname_handle,arg0);
     catch 
-            [out0]=arrayfun(fnname,arg0);
+            [out0]=arrayfun(fnname_handle,arg0);
     end
 
-    if(any(iscomplex(out0))||any(iscomplex(out1)))
+    if(any(~isreal(out0))||any(~isreal(out1)))
       scale=2;
     else
       scale=1;
@@ -56,48 +58,60 @@ function retstr = gendata(fnname, uname, lang, arg0, arg1)
     if(~isempty(out1))
       writeData(fp,out0,'expected_data','0',lang);
       writeData(fp,out1,'expected_data','1',lang);
-      retstr = [retstr,"double[] y0 = new double[",num2str(scale),"*expected_data0.length];\n"];
-      retstr = [retstr,"double[] y1 = new double[",num2str(scale),"*expected_data1.length];\n"];
-      retstr = [retstr,"_izy.",filetag,uname,"(new int[] {n_in}, in_data, zero, y0, zero, y1, zero);\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data0));\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y1, expected_data1));\n"];
+      retstr = [retstr,'double[] y0 = new double[',num2str(scale),'*expected_data0.length];\n'];
+      retstr = [retstr,'double[] y1 = new double[',num2str(scale),'*expected_data1.length];\n'];
+      retstr = [retstr,'_izy.',filetag,uname,'(new int[] {n_in}, in_data, zero, y0, zero, y1, zero);\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data0));\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y1, expected_data1));\n'];
     else
       writeData(fp,out0,'expected_data','',lang);
-      retstr = [retstr,"double[] y0 = new double[expected_data.length];\n"];
-      retstr = [retstr,"_izy.",filetag,uname,"(new int[] {n_in}, in_data, zero, y0, zero);\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data));\n"];
+      retstr = [retstr,'double[] y0 = new double[expected_data.length];\n'];
+      retstr = [retstr,'_izy.',filetag,uname,'(new int[] {n_in}, in_data, zero, y0, zero);\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data));\n'];
     end
   elseif(nargin==5)  
-    writeData(fp,arg0,'in_data','0',lang);
-    writeData(fp,arg1,'in_data','1',lang);   
-
+    
+      if(strcmp(upper(uname(end)),'X'))% functions ending in "x" need 1 const value 
+        writeData(fp,arg0,'in_data','0',lang);          
+        writeData(fp,arg1(1),'in_data','1',lang);   
+      elseif(strcmp(upper(uname(1)),'X'))% functions starting in "x" need 1 const value 
+        writeData(fp,arg0(1),'in_data','0',lang);   
+        writeData(fp,arg1,'in_data','1',lang);
+      elseif (strcmp(upper(uname),'power'))
+        writeData(fp,arg0,'in_data','0',lang);   
+        writeData(fp,arg1(1),'in_data','1',lang);   
+      else
+        writeData(fp,arg0,'in_data','0',lang);   
+        writeData(fp,arg1,'in_data','1',lang);   
+      end
+        
     out1=[];
     out0=[];
     try
-            [out0,out1]=arrayfun(fnname,arg0,arg1);
+            [out0,out1]=arrayfun(fnname_handle,arg0,arg1);
     catch
-            [out0]=arrayfun(fnname,arg0,arg1);
+            [out0]=arrayfun(fnname_handle,arg0,arg1);
     end
 
     fprintf(fp,'int n_expected = %d;\n',numel(out0));
     if(~isempty(out1))
       writeData(fp,out0,'expected_data','0',lang);
       writeData(fp,out1,'expected_data','1',lang);
-      retstr = [retstr,"double[] y0 = new double[",num2str(scale),"*expected_data0.length];\n"];
-      retstr = [retstr,"double[] y1 = new double[",num2str(scale),"*expected_data1.length];\n"];
-      retstr = [retstr,"_izy.",filetag,uname,"(new int[] {n_in}, in_data0, zero, in_data1, zero, y0, zero, y1, zero);\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data0));\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y1, expected_data1));\n"];
+      retstr = [retstr,'double[] y0 = new double[',num2str(scale),'*expected_data0.length];\n'];
+      retstr = [retstr,'double[] y1 = new double[',num2str(scale),'*expected_data1.length];\n'];
+      retstr = [retstr,'_izy.',filetag,uname,'(new int[] {n_in}, in_data0, zero, in_data1, zero, y0, zero, y1, zero);\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data0));\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y1, expected_data1));\n'];
     else
       writeData(fp,out0,'expected_data','',lang);
-      retstr = [retstr,"double[] y0 = new double[expected_data.length];\n"];
-      retstr = [retstr,"_izy.",filetag,uname,"(new int[] {n_in}, in_data0, zero, in_data1, zero, y0, zero);\n"];
-      retstr = [retstr, "assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data));\n"];
+      retstr = [retstr,'double[] y0 = new double[expected_data.length];\n'];
+      retstr = [retstr,'_izy.',filetag,uname,'(new int[] {n_in}, in_data0, zero, in_data1, zero, y0, zero);\n'];
+      retstr = [retstr, 'assertTrue(FPEquals.fuzzyEqualsDynamicTol(y0, expected_data));\n'];
     end
   else
-          error("Too many args")
+          error('Too many args')
   end
-  retstr = [retstr,"\n};\n\n"];
+  retstr = [retstr,'\n};\n\n'];
   fclose(fp);
 end % function
 
@@ -158,15 +172,32 @@ function ret = nearbyint(arg0)
         end
 end
 
+% bad, compensates for no stats package
+function ret = normcdf(arg0)
+    ret = erfc(arg0/(-sqrt(2)))/2;
+end
+
+function ret = cbrt(arg0)
+    if(arg0>0)   
+        ret = power(arg0,1/3);
+    else
+        ret = -power(-arg0,1/3);
+    end
+end
+
+function ret = arg(arg0)
+    ret = atan2 (imag(arg0), real(arg0));
+end
+
 function [] = writeData(fp,out,name,val,lang);
-    if(~any(iscomplex(out)))
+    if(~any(~isreal(out)))
       fmt = '%24.15e';
       if(strcmp(lower(lang),'c'))
         declstr=['static double ',name,val,'[] = {'];
       elseif(strcmp(lower(lang),'java'))
         declstr=['double ',name,val,'[] = {'];
       else
-        error("unknown language",lang);
+        error('unknown language',lang);
       end
     else      % complex numbers
       if(strcmp(lower(lang),'c'))
@@ -176,29 +207,29 @@ function [] = writeData(fp,out,name,val,lang);
         declstr=['double ',name,val,'[] = {'];
         fmt = '%24.15e, %24.15e';
       else
-        error("unknown language",lang)
+        error('unknown language',lang)
       end
     end
 
     fprintf(fp,'%s\n',declstr);
-    if(~any(iscomplex(out)))
+    if(~any(~isreal(out)))
       for k=1:numel(out)-1
-        fprintf(fp,fmt,out(k))
+        fprintf(fp,fmt,out(k));
         fprintf(fp,',');
         if(~mod(k,10))
           fprintf(fp,'\n');
         end
       end
-      fprintf(fp,fmt,out(end))
+      fprintf(fp,fmt,out(end));
     else
       for k=1:numel(out)-1
-        fprintf(fp,fmt,[real(out(k)),imag(out(k))])
+        fprintf(fp,fmt,[real(out(k)),imag(out(k))]);
         fprintf(fp,',');
         if(~mod(k,10))
           fprintf(fp,'\n');
         end
       end
-        fprintf(fp,fmt,[real(out(end)),imag(out(end))])
+        fprintf(fp,fmt,[real(out(end)),imag(out(end))]);
     end
 
     fprintf(fp,'\n};\n');
--- src/test/generate_c_includes.m
+++ src/test/generate_c_includes.m
@@ -1,5 +1,5 @@
 %% main code
 clear all; close all; clc;
 rand('state',0);
-gen_per_lang("c");
+gen_per_lang('c');
 
