00d46c2f8a00ea991ddf8745b61f48a8a66cb6b4
Stuart Archibald <stuart@opengamma.com>
2014-07-11 16:47:49 +0100

    [MAT-425] make use of typedef'd types consistent.
    Swap double for real8 to be consistent with `complex double` and
    complex16 usage in this header.

--- include/libizy/izymacros.h
+++ include/libizy/izymacros.h
@@ -31,13 +31,13 @@ typedef int32_t int4;
 
 
 /* double precision definitions */
-#define _ONEARGONERESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const double * arg0, const int * offsetarg0, double * out0, const int * offsetout0);
+#define _ONEARGONERESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const real8 * arg0, const int * offsetarg0, real8 * out0, const int * offsetout0);
 
-#define _ONEARGTWORESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const double * arg0, const int * offsetarg0, double * out0, const int * offsetout0, double * out1, const int * offsetout1);
+#define _ONEARGTWORESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const real8 * arg0, const int * offsetarg0, real8 * out0, const int * offsetout0, real8 * out1, const int * offsetout1);
 
-#define _TWOARGONERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const double * arg0, const int * offsetarg0, const double * arg1, const int * offsetarg1, double * out0, const int * offsetout0);
+#define _TWOARGONERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const real8 * arg0, const int * offsetarg0, const real8 * arg1, const int * offsetarg1, real8 * out0, const int * offsetout0);
 
-#define _TWOARGONEISCONSTONERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const double * arg0, const int * offsetarg0, const double * arg1, const int * offsetarg1, double * out0, const int * offsetout0);
+#define _TWOARGONEISCONSTONERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const real8 * arg0, const int * offsetarg0, const real8 * arg1, const int * offsetarg1, real8 * out0, const int * offsetout0);
 
 #define _DVECTORONEARGONERESULTFUNCTION(_NAME) _ONEARGONERESULTFUNCTION(vd_, _NAME)
 #define _DVECTORONEARGTWORESULTFUNCTION(_NAME) _ONEARGTWORESULTFUNCTION(vd_, _NAME)
@@ -45,14 +45,14 @@ typedef int32_t int4;
 #define _DVECTORTWOARGONEISCONSTONERESULTFUNCTION(_NAME) _TWOARGONEISCONSTONERESULTFUNCTION(vd_, _NAME)
 
 
-/* double complex definitions */
+/* real8 complex definitions */
 #define _ZONECOMPLEXARGONECOMPLEXRESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, complex16 * out0, const int * offsetout0);
 
-#define _ZONECOMPLEXARGONEDOUBLERESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, double * out0, const int * offsetout0);
+#define _ZONECOMPLEXARGONEDOUBLERESULTFUNCTION(_VECTORNAME,_NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, real8 * out0, const int * offsetout0);
 
 #define _ZTWOCOMPLEXARGONECOMPLEXRESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, const complex16 * arg1, const int * offsetarg1, complex16 * out0, const int * offsetout0);
 
-#define _ZTWOCOMPLEXARGONEDOUBLERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, const complex16 * arg1, const int * offsetarg1, double * out0, const int * offsetout0);
+#define _ZTWOCOMPLEXARGONEDOUBLERESULTFUNCTION(_VECTORNAME, _NAME) void _VECTORNAME##_NAME(const int * count, const complex16 * arg0, const int * offsetarg0, const complex16 * arg1, const int * offsetarg1, real8 * out0, const int * offsetout0);
 
 #define _ZVECTORONECOMPLEXARGONECOMPLEXRESULTFUNCTION(_NAME) _ZONECOMPLEXARGONECOMPLEXRESULTFUNCTION(vz_, _NAME)
 #define _ZVECTORONECOMPLEXARGONEDOUBLERESULTFUNCTION(_NAME) _ZONECOMPLEXARGONEDOUBLERESULTFUNCTION(vz_, _NAME)
